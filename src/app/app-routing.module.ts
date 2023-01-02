af4950232b37ca0463cb3304d66ec1a4f5a6a0c9
TahaLechgar <tahamr08@gmail.com>
2023-01-02 14:04:07 +0100

    update app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent, WelcomeComponent } from './home/pages';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: WelcomeComponent},
+  {path: "about", component: AboutusComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    HomeModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
