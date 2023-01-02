3845b02dbd2ec277e626c754153edb5cab7409f3
TahaLechgar <tahamr08@gmail.com>
2023-01-02 14:03:16 +0100

    add shared module that holds shared functionnalities

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ], 
+  exports : [
+    NavbarComponent,
+    FooterComponent
+  ]
+})
+export class SharedModule { }
