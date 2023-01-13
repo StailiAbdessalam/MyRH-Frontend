5b5111cfe19b22e51cb605596184f7de9753215b
TahaLechgar <tahamr08@gmail.com>
2023-01-13 16:21:47 +0100

    add CallbackComponent that gets redirected to after logging with Google to retrieve access token

---
+++ src/app/callback/callback.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {SecurityService} from "../core";
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-callback',
+  templateUrl: './callback.component.html',
+  styleUrls: ['./callback.component.css']
+})
+export class CallbackComponent implements OnInit {
+
+  constructor(private securityService: SecurityService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe({
+      next: (p) => {
+
+        this.securityService.fetchToken(p['code'], p['state']).subscribe(data => {
+          this.securityService.updateToken(data.accessToken)
+          this.router.navigate(['/jobs'])
+        })
+      }
+    })
+  }
+
+}
