6e7a7515f404ab9666730d524b9577da7b2a5464
TahaLechgar <tahamr08@gmail.com>
2023-01-13 16:17:35 +0100

    Add SecurityService that handle security logic

---
+++ src/app/core/services/securtiy/security.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SecurityService {
+  private authorizeEndpoint = '/oauth2/authorization/google'
+  private tokenEndpoint     = '/login/oauth2/code/google'
+  private baseUrl           = environment.baseUrl
+  private tokenKey          = 'key'
+
+  constructor(private http: HttpClient) { }
+
+  login(){
+    window.open(this.baseUrl + this.authorizeEndpoint, '_self')
+  }
+
+  updateToken(token: string) {
+    localStorage.setItem(this.tokenKey, token)
+  }
+
+  fetchToken(code: string, state: string): Observable<any> {
+    return  this.http.get(this.baseUrl + this.tokenEndpoint + "?code=" + code + "&state=" + state);
+  }
+
+  getToken(){
+    return localStorage.getItem(this.tokenKey)
+  }
+
+  isLoggedIn(): boolean{
+    const token = this.getToken();
+    return token != null
+  }
+
+}
